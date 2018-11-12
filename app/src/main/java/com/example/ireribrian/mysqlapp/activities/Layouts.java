e71a8dad3fe98400f64aaab3b1cc221b515abc77
ireri-mwenda <bmugendi98@gmail.com>
2018-11-12 11:52:25 +0300

    bug fix

--- app/src/main/java/com/example/ireribrian/mysqlapp/activities/Layouts.java
+++ app/src/main/java/com/example/ireribrian/mysqlapp/activities/Layouts.java
@@ -7,8 +7,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.ireribrian.mysqlapp.R;
 import com.example.ireribrian.mysqlapp.session.SessionManager;
@@ -17,10 +20,15 @@ import com.example.ireribrian.mysqlapp.ui.CvProfileTwo;
 import com.example.ireribrian.mysqlapp.ui.SocialMediaOne;
 import com.example.ireribrian.mysqlapp.ui.SocialMediaTwo;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class Layouts extends AppCompatActivity {
+
     SessionManager sessionManager;
 
     String [] Layouts = {"CV Profile Layout", "Social Media Sample 1", "Social Media Sample 2", "Professional CV Layout"};
+    int [] Images = {R.drawable.login, R.drawable.register, R.drawable.splash, R.drawable.home};
+    String [] Rating = {"9.7", "9.4", "9.8", "9.9"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,28 +42,65 @@ public class Layouts extends AppCompatActivity {
         sessionManager = new SessionManager(this);
 
         ListView profile_list_view = (ListView) findViewById(R.id.layout_list);
+
+        CustomAdapter customAdapter = new CustomAdapter();
+        profile_list_view.setAdapter(customAdapter);
+
         profile_list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                if(position == 0){
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i == 0){
                     final Intent cv_profile_intent = new Intent(Layouts.this, CvProfile.class);
                     startActivity(cv_profile_intent);
                 }else
-                    if(position == 1){
+                    if(i == 1){
                     final Intent social_mediaone_profile_intent = new Intent(Layouts.this, SocialMediaOne.class);
                     startActivity(social_mediaone_profile_intent);
                     }else
-                    if(position == 2){
+                    if(i == 2){
                         final Intent social_mediatwo_profile_intent = new Intent(Layouts.this, SocialMediaTwo.class);
                         startActivity(social_mediatwo_profile_intent);
                     }else
-                    if(position == 3){
+                    if(i == 3){
                         final Intent cv_profile_two_intent = new Intent(Layouts.this, CvProfileTwo.class);
                         startActivity(cv_profile_two_intent);
                     }
 
             }
         });
+    }
+
+    class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return Images.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.customlayouts, null);
+
+            CircleImageView icon = (CircleImageView) findViewById(R.id.profile_layout_icon);
+            TextView name = (TextView) findViewById(R.id.profile_layout_name);
+            TextView rating = (TextView) findViewById(R.id.profile_layout_rating);
+
+            icon.setImageResource(Images[i]);
+            name.setText(Layouts[i]);
+            rating.setText(Rating[i]);
+
+            return view;
+        }
     }
 
     @Override
